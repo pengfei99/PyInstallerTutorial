c7d19565e5de1be0130d2c911bcf0e4cb4743711
pengfei liu <liu.pengfei@hotmail.fr>
2025-03-19 16:10:03 +0100

    update unit test

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,5 +1,10 @@
-from casd_hub.resources import get_icon_path
+from casd_hub.resources import get_icon_path, get_config
+
 
 def test_get_icon_path():
     icon_path = get_icon_path("bouclier.ico")
+    print(icon_path)
+
+def test_get_config():
+    icon_path = get_config()
     print(icon_path)
